142c7aafc765e5f854f13a8a01454c26ee3f47ba
Ivan Kostiuk <ivan.kostiuk@motorolasolutions.com>
2020-01-25 20:30:01 +0100

    first commit

---
+++ WebApplication4/Controllers/CategoryController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication4.Models;
+
+namespace WebApplication4.Controllers
+{
+    
+    public class CategoryController : ApiController
+    {
+        public MyModel _category = new MyModel();
+
+        public CategoryController()
+        {
+           List<Task> tasks = new List<Task>();
+            _category.Name = "TODO";
+            _category.addTask("Kupic_wode", "11.12.21");
+            _category.addTask("Kupic_samochod", "11.12.21");
+            _category.addTask("Kupic_rower", "11.12.21");
+
+        }
+
+        // GET: api/Category
+        public MyModel Get()
+        {
+            return _category;
+        }
+
+        // GET: api/Category/5
+        public Task Get(string name)
+        {
+            foreach(var x in _category.Tasks)
+            {
+                if( x.Name == name)
+                {
+                    return x;
+                }
+            }
+            return new Task { Name = "ERROR", Date = "ERROR" };
+        }
+
+        // POST: api/Category
+        public void Post(Task task)
+        {
+            //Check if this categoty alregy exist
+            foreach(var  x in _category.Tasks)
+            {
+                if(x.Name == task.Name)
+                {
+                    //It exist already
+                    return;
+                }
+            }
+            _category.Tasks.Add(task);
+
+        }
+
+        // PUT: api/Category/5
+        public void Put(string name, Task nowy)
+        {
+            Task tmp = _category.getTask(name);
+            tmp.Name = nowy.Name;
+            tmp.Date = nowy.Date;
+        }
+
+        // DELETE: api/Category/5
+        public void Delete(string name)
+        {
+            foreach(var x in _category.Tasks)
+            {
+                if(x.Name == name)
+                {
+                    _category.Tasks.Remove(x);
+                }
+            }
+        }
+    }
+}
---
+++ WebApplication4/Models/MyModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class Task
+    {
+        public String Name { get; set; } = "";
+        public String Date { get; set; } = "";
+    }
+
+    public class MyModel
+    {
+        public List<Task> Tasks { get; set; } = new List<Task>();
+        public String Name { get; set; } = "";
+        public MyModel()
+        {
+        }
+
+        public void addTask(String name, String date)
+        {
+            Tasks.Add(new Task() { Name = name, Date = date });
+        }
+        public Task getTask(String name)
+        {
+            Task res;
+            foreach(var x in Tasks)
+            {
+                if(x.Name == name)
+                {
+                    return x;
+                }
+            }
+            return new Task { Name = "ERROR", Date = "ERROR" };
+
+        }
+
+    }
+}
